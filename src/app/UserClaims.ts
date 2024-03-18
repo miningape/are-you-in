509551cdd444ceab0a58c7a32a7e65c52ce05cc7
Kyle Johnson <miningape@gmail.com>
2024-03-18 17:46:05 +0100

    fix: optional fields on login

--- src/app/UserClaims.ts
+++ src/app/UserClaims.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const UserClaims = z.object({
-  given_name: z.string(),
-  family_name: z.string(),
+  given_name: z.optional(z.string()),
+  family_name: z.optional(z.string()),
   nickname: z.string(),
   picture: z.string(),
   email: z.string(),
